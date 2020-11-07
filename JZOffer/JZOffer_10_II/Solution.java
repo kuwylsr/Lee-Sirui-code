ceedc09a5303db820e50afbd1f7558c99e760095
1160300610 <17745109091@163.com>
2020-11-07 15:43:22 +0800

    2020-11-7

--- JZOffer/JZOffer_10_II/Solution.java
+++ JZOffer/JZOffer_10_II/Solution.java
@@ -19,7 +19,7 @@ public class Solution {
             sum -= p;
         }   
     }
-    public int numWays(int n) {
+    public int numWays1(int n) {
         plan_num = 0;
         int[] pace = {1,2};
         backTrace(n,pace,0);
@@ -27,10 +27,42 @@ public class Solution {
         return ans;
     }
 
-    
+    // 动态规划 : 
+    public int numWays2(int n) {
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1 ; i++){
+            dp[i] = (int)((dp[i - 1] + dp[i - 2]) % (1e9 + 7));
+        }
+        int ans = dp[n];
+        return ans;
+    }
+
+    // 动态规划 : O(1) 空间 
+    public int numWays3(int n) {
+        if(n == 0 || n == 1){
+            return 1;
+        }
+        int pre1 = 1;
+        int pre2 = 1;
+        int sum = 0;
+        for (int i = 2; i < n + 1 ; i++){
+            sum = (int)((pre1 + pre2)% (1e9 + 7));
+            pre1 = pre2;
+            pre2 = sum;
+        }
+        int ans = sum;
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         Solution S = new Solution();
-        int ans = S.numWays(7);
+        int ans = S.numWays3(7);
         System.out.println(ans);
     }
 }
---
+++ Summary/DynamicProgramming/LinearDynamicProgramming/SingleString/LongestIncreasingSubsequence_300/Solution.java
@@ -0,0 +1,45 @@
+package DynamicProgramming.LinearDynamicProgramming.SingleString.LongestIncreasingSubsequence_300;
+
+public class Solution {
+
+    // 动态规划
+    public int lengthOfLIS(int[] nums) {
+        int max = 1;
+        int len = nums.length;
+        if(len == 0 ){
+            return 0;
+        }
+        int[] dp = new int[len];
+        dp[0] = 1;
+        
+        for(int i = 1; i < len; i++){
+            int tmp_max = Integer.MIN_VALUE;
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    if(dp[j] > tmp_max){
+                        tmp_max = dp[j];
+                    }
+                }
+            }
+            if(tmp_max == Integer.MIN_VALUE){
+                dp[i] = 1;
+            }else{
+                dp[i] = tmp_max + 1;
+            }
+
+            if(dp[i] > max){
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+    // 贪心 + 二分查找
+    public int lengthOfLIS2(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
