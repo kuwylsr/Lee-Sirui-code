934e3435e7968b3ff4a96298cc8a5f2f942e82d3
1160300610 <17745109091@163.com>
2021-01-02 22:08:26 +0800

    2021-1-2

---
+++ Summary/Algorithm/BinarySearch/FindMinimumInRotatedSortedArray_153/Solution.java
@@ -0,0 +1,36 @@
+package Algorithm.BinarySearch.FindMinimumInRotatedSortedArray_153;
+
+public class Solution {
+    public int findMin(int[] nums) {
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+
+        if(len == 1){
+            return nums[0];
+        }
+
+        if(nums[0] < nums[len-1]){ //没有旋转
+            return nums[0];
+        }
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+        
+            if(nums[mid] > nums[mid+1]){
+                return nums[mid+1];
+            }
+
+            if(nums[mid-1] > nums[mid]){
+                return nums[mid];
+            }
+
+            if(nums[mid] >= nums[0]){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
